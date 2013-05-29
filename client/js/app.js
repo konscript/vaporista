290186ababf936e8bee4bb23eb6e768baab897ea
laander <la@laander.com>
2013-05-29 14:37:04 +0200

    new about page, updated slider style and minor tweaks

--- client/js/app.js
+++ client/js/app.js
@@ -4,5 +4,6 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: '/client/tmpl/home.html', controller: 'HomeCtrl'});
+    $routeProvider.when('/about', {templateUrl: '/client/tmpl/about.html'});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
