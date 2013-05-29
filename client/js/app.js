2a53b342ad2d39408db24ff57e7ee1054e9733ac
laander <la@laander.com>
2013-05-29 13:51:56 +0200

    moved files and dirs

--- client/js/app.js
+++ client/js/app.js
@@ -3,6 +3,6 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: '/client/partials/home.html', controller: 'HomeCtrl'});
+    $routeProvider.when('/home', {templateUrl: '/client/tmpl/home.html', controller: 'HomeCtrl'});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
